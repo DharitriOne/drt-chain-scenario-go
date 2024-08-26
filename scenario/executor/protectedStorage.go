d33490bd12593c1191e29f8a9f618eea498c8f03
xfactor-toml <xfactor.toml@gmail.com>
2024-08-26 05:31:27 -0400

    feat: release initial version

---
+++ scenario/executor/protectedStorage.go
@@ -0,0 +1,6 @@
+package scenexec
+
+import "github.com/DharitriOne/drt-chain-core-go/core"
+
+// RewardKey is the storage key where the protocol writes when sending out rewards.
+const RewardKey = core.ProtectedKeyPrefix + "reward"
